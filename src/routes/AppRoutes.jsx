c8b77f42786c9fe33242315dc865ec4cf4b12a60
yogiefani <yogiefaniyc@gmail.com>
2025-01-10 23:12:10 +0700

    feat: add visi and misi page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import VisiMisi from "../pages/VisiMisi";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/visi-misi" element={<VisiMisi />} />
     </Routes>
   );
 };
