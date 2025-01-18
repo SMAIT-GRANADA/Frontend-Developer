f01cbab292f7352994842222a640fd4e2b83cd21
yogiefani <yogiefaniyc@gmail.com>
2025-01-19 00:42:18 +0700

    feat: add parallax in mapregist

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -62,7 +62,7 @@ export default function Stats() {
   ];
 
   return (
-    <div className="container mx-auto px-4 lg:px-48 py-16">
+    <div className="w-full mx-auto px-4 lg:px-48 py-16 bg-white">
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {stats.map((stat, index) => (
           <StatsCard
