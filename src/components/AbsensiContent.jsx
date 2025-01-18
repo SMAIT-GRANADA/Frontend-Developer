670ff604a6f3b3604427b19feff1c2769c0f428d
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-01-18 22:17:51 +0700

    add(student page): added page student

--- src/components/AbsensiContent.jsx
+++ src/components/AbsensiContent.jsx
@@ -1,14 +1,14 @@
 const AbsensiContent = () => {
   return (
-    <div className="p-6 max-w-6xl mx-auto py-20">
+    <div className="p-6 max-w-6xl mx-auto py-20 border border-gray-200 shadow-sm rounded-lg px-8">
       <h1 className="text-2xl font-bold mb-2">Absensi</h1>
       <h2 className="text-emerald-600 text-xl mb-4">Hi, Rafly</h2>
-      <p className="text-gray-600 mb-8">
+      <p className="text-gray-600 mb-8 border-b border-gray-200 pb-4">
         Halaman ini membantu kamu dalam memantau laporan absensi kamu selama
         Sekolah!
       </p>
 
-      <div className="bg-white rounded-lg shadow-sm p-6 mb-6">
+      <div className="bg-white rounded-lg shadow-lg p-6 mb-6 border">
         <div className="space-y-4">
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -30,7 +30,7 @@ const AbsensiContent = () => {
         </button>
       </div>
 
-      <div className="mt-6 bg-white rounded-lg shadow-sm p-6 min-h-[400px]"></div>
+      <div className="mt-6 bg-white rounded-lg shadow-lg border p-6 min-h-[400px]"></div>
     </div>
   );
 };
--- src/pages/StudentDashboardPage.jsx
+++ src/pages/StudentDashboardPage.jsx
@@ -2,27 +2,31 @@ import React, { useState } from "react";
 import { Menu } from "lucide-react";
 import SidebarStudent from "../components/StudentSidebar";
 import AbsensiContent from "../components/AbsensiContent";
+import Navbar from "../components/Navbar";
 
 const StudentDashboardPage = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      <SidebarStudent
-        isOpen={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}
-      />
-
-      <button
-        onClick={() => setIsDrawerOpen(true)}
-        className="lg:hidden fixed top-4 left-4 p-2 bg-emerald-700/80 rounded-lg text-white z-20"
-      >
-        <Menu className="w-6 h-6" />
-      </button>
-
-      <main className="flex-1">
-        <AbsensiContent />
-      </main>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="flex pt-28">
+        <SidebarStudent
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
+        />
+        <button
+          onClick={() => setIsDrawerOpen(true)}
+          className={`lg:hidden fixed top-32 left-4 p-2 bg-emerald-700/80 rounded-lg text-white transition-all duration-300 ${
+            isDrawerOpen ? "z-0" : "z-[60]"
+          }`}
+        >
+          <Menu className="w-6 h-6" />
+        </button>
+        <main className="flex-1 p-6 mt-4">
+          <AbsensiContent />
+        </main>
+      </div>
     </div>
   );
 };
