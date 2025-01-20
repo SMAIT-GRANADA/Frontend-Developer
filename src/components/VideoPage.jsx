40592d54a4ad90f7baaf2e9cb0c475195f1afb89
yogiefani <yogiefaniyc@gmail.com>
2025-01-21 01:25:51 +0700

    fix: wavehome overflow

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -3,12 +3,12 @@ import imageYoutube from "../assets/VideoGranada.webp";
 
 const VideoPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl w-full">
+    <div className="w-full bg-white py-10 relative z-10">
+      <div className="container mx-auto px-4 lg:px-48 ">
         <h1 className="text-2xl font-bold text-center mb-4">
           Video SMA IT Granada Samarinda Tahun 2024
         </h1>
-        <p className="text-gray-700 mb-6">
+        <p className="text-gray-700 mb-6 text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
@@ -16,7 +16,7 @@ const VideoPage = () => {
           <img
             src={imageYoutube}
             alt="Video SMA IT Granada Samarinda"
-            className="rounded-lg"
+            className="w-full max-w-4xl rounded-lg shadow-lg"
           />
         </div>
       </div>
