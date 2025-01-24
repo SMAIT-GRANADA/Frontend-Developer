019b916ebed05f02b001aa3e68933044a4652ff9
yogiefani <yogiefaniyc@gmail.com>
2025-01-24 23:31:31 +0700

    fix: news page update skeleton

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,69 +1,52 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import MainNews from "../components/MainNews";
-import SideNews from "../components/SideNews";
-import MainNewsSkeleton from "../components/Skeleton/MainNewsSkeleton";
-import SideNewsSkeleton from "../components/Skeleton/SideNewsSkeleton";
 import { useNewsQuery } from "../hooks/useNewsQuery";
 import arrowRight from "../assets/arrow-right-circle.svg";
-import { useNavigate } from "react-router-dom";
+import NewsCard from "../components/NewsCard";
+import NewsCardSkeleton from "../components/Skeleton/NewsCardSkeleton";
+import NewsGrid from "../components/NewsGrid";
+import NewsGridSkeleton from "../components/Skeleton/NewsGridSkeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const News = () => {
   const { data: newsData, isLoading } = useNewsQuery();
-  const navigate = useNavigate();
-
-  const handleToNewsPage = () => {
-    navigate("/news");
-  };
 
   return (
     <>
       <Navbar />
-      <div className="pt-28 lg:pt-32">
+      <div className="pt-28 lg:pt-32 mt-8">
         <div className="max-w-6xl mx-auto px-4 py-8">
-          <header className="mb-8 py-8 relative">
+          <header className="mb-8">
             <h1 className="text-4xl font-bold mb-4">BERITA GRANADA</h1>
             <p className="text-gray-600">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            <div className="absolute bottom-0 left-0 right-0 h-px bg-[#949593]">
-              <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-1 h-1 bg-[#949593] rounded-full"></div>
-              <div className="absolute top-1/2 right-0 transform -translate-y-1/2 w-1 h-1 bg-[#949593] rounded-full"></div>
+            <div className="relative mt-8 h-px bg-[#949593]">
+              <div className="absolute top-1/2 left-0 -translate-y-1/2 w-1 h-1 bg-[#949593] rounded-full" />
+              <div className="absolute top-1/2 right-0 -translate-y-1/2 w-1 h-1 bg-[#949593] rounded-full" />
             </div>
           </header>
 
-          <main className="grid lg:grid-cols-3 gap-8">
+          <main className="space-y-12">
             {isLoading ? (
               <>
-                <MainNewsSkeleton />
-                <div className="lg:col-span-1">
-                  {[1, 2, 3].map((index) => (
-                    <SideNewsSkeleton key={index} />
-                  ))}
-                </div>
+                <NewsCardSkeleton isMain={true} />
+                <NewsGridSkeleton />
+                <NewsGridSkeleton />
               </>
             ) : (
               <>
-                {newsData?.data[0] && <MainNews news={newsData.data[0]} />}
-                <div className="lg:col-span-1">
-                  {newsData?.data.slice(1, 4).map((news, index) => (
-                    <SideNews
-                      key={news.id}
-                      news={news}
-                      isLast={index === newsData.data.slice(1, 4).length - 1}
-                    />
-                  ))}
-                </div>
+                <NewsCard news={newsData?.data[0]} isMain={true} />
+                <NewsGrid news={newsData?.data} startIndex={1} endIndex={4} />
+                <NewsGrid news={newsData?.data} startIndex={4} endIndex={7} />
               </>
             )}
           </main>
-          <div className="flex justify-center">
-            <button
-              className="bg-yellow-400 px-4 py-2 rounded-full text-sm font-medium hover:bg-yellow-500 transition-colors flex items-center"
-              onClick={handleToNewsPage}
-            >
-              Lihat Semua
+
+          <div className="flex justify-center mt-8">
+            <button className="bg-yellow-400 px-4 py-2 rounded-full text-sm font-medium hover:bg-yellow-500 transition-colors flex items-center">
+              Lebih lainnya
               <img
                 src={arrowRight}
                 alt="Arrow Right"
