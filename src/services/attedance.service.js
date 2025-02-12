86b04b63832badeae549a740dd2a797e1f1d07f5
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-02-13 01:40:08 +0700

    feat: attendance and get attendance

---
+++ src/services/attedance.service.js
@@ -0,0 +1,15 @@
+import axiosInstance from "../api/axiosInstance";
+
+export const checkIn = async ({ photoBase64, latitude, longitude }) => {
+  const response = await axiosInstance.post("/attendance/check-in", {
+    photoBase64,
+    latitude,
+    longitude,
+  });
+  return response.data;
+};
+
+export const getTodayAttendance = async () => {
+  const response = await axiosInstance.get("/attendance/today");
+  return response.data;
+};
