34747fb27a8ec9c0d51a32c99d2b1825afe2a68f
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-03-07 22:58:49 +0700

    update sidebar components to change avatar background color from gray to emerald

--- src/components/Sidebar/AdminSidebar.jsx
+++ src/components/Sidebar/AdminSidebar.jsx
@@ -59,7 +59,7 @@ const AdminSidebar = ({ isOpen, setIsOpen, setActiveMenu }) => {
         <div className="flex-none p-6">
           <div className="flex justify-between items-center mb-8">
             <div className="flex items-center gap-4">
-              <div className="w-12 h-12 bg-gray-400 rounded-full overflow-hidden">
+              <div className="w-12 h-12 bg-emerald-700 rounded-full overflow-hidden">
                 <img
                   src={avatar}
                   alt="Admin"
